1cce32d010383f01691f567c4c177e20eaa74f64
wjdals1217 <wjdals1217@gmail.com>
2023-06-13 15:31:00 +0900

    Ch01 수업예제

---
+++ Ch01/src/sub1/Hello.java
@@ -0,0 +1,26 @@
+package sub1;
+
+/**
+ * 날짜 : 2023/06/12
+ * 이름 : 최정민
+ * 내용 : Ch01.Java 개발환경구축
+ */
+public class Hello {
+	
+	// 프로그램 진입점
+	public static void main(String[] args) {
+		
+		// 기본출력
+		System.out.println("Hello World~");
+		System.out.println("Hello Java~");
+		
+		// 한줄출력
+		System.out.print("Hello ");
+		System.out.print("Korea~");
+		
+		//개행처리(줄바꿈처리)
+		System.out.print("\n");
+		System.out.print("Hello Busan~");
+	}
+	
+}
