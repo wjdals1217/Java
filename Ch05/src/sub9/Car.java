19faba2db82ecd3fb910b0e33f88afd879d1434b
wjdals1217 <wjdals1217@gmail.com>
2023-07-03 11:45:48 +0900

    수정

---
+++ Ch05/src/sub9/Car.java
@@ -0,0 +1,11 @@
+package sub9;
+
+public interface Car {
+	
+	public abstract void speedUp(int speed);
+	public abstract void speedDown(int speed);
+	public void show(); // abstract는 생략가능함(추상 메서드만 가질 수 있기 때문)
+	
+	
+	
+}
