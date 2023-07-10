428cb8fb236ca0ce1c6ede9a7668485c30f68a42
wjdals1217 <wjdals1217@gmail.com>
2023-07-10 10:00:14 +0900

    백준코딩테스트

---
+++ BaekjunCodingTest/src/step1/Test08.java
@@ -0,0 +1,21 @@
+package step1;
+
+import java.util.Scanner;
+
+/**
+ * 10108번 1998년생인 내가 태국에서는 2541년생?!
+ */
+
+public class Test08 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int y = sc.nextInt();
+		
+		if(y>=1000 && y<=3000) {
+			
+		}
+	}
+
+}
