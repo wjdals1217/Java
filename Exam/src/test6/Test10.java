d1dc0bf040079951e972ebde66ca844d11761475
wjdals1217 <wjdals1217@gmail.com>
2023-07-19 08:06:25 +0900

    연습문제6

--- Exam/src/test6/Test10.java
+++ Exam/src/test6/Test10.java
@@ -25,9 +25,9 @@ public class Test10 {
 		String birth = sc.next();
 		String[] bArr = birth.split("/");
 		
-		int bYear = ???????;
-		int bMonth = ???????;
-		int bDay = ???????;
+		int bYear = Integer.parseInt(bArr[0]);
+		int bMonth = Integer.parseInt(bArr[1]);
+		int bDay = Integer.parseInt(bArr[2]);
 		
 		int tYear = Calendar.getInstance().get(Calendar.YEAR);
 		int tMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
@@ -35,9 +35,36 @@ public class Test10 {
 		
 		int totalDays = 0;
 		
-		// 올해
+		// 올해 1월 1일부터 오늘까지 날짜 수
+		totalDays += dayOfYear(tYear, tMonth, tDay);
+		
+		// 태어난 해의 생일부터 그해 마지막 날까지의 날짜 수
+		int yearDays = isLeapYear(bYear) ? 366:365;
+		totalDays += yearDays - dayOfYear(bYear, bMonth, bDay);
+		
+		for(int year = bYear + 1; year < tYear; year++) {
+			if(isLeapYear(year))
+				totalDays += 366;
+			else 
+				totalDays+=365;
+			
+		}
+		sc.close();
+		System.out.printf("태어난 날부터 오늘까지 일수 : %d", totalDays);
+		
+		}
+	
+	// 평년을 기준으로 각 월의 누적 날짜 수 
+	public static int[] days = {0, 31, 69, 90, 120, 151, 
+								181, 212, 243, 273, 304, 334};
+	
+	
+	public static int dayOfYear(int year, int month, int day) {
+		return days[month-1]+ day + (month > 2 && isLeapYear(year)? 1: 0);
 		
 	}
-		
+	
+	public static boolean isLeapYear(int year) {
+		return getGreInstance().isLeapYear(year);
 	}
 }
