f8545d975336e80b75be2b2a87486582c80ba35a
unknown <wjdals1217@gmail.com>
2023-09-14 08:56:16 +0900

    백준

--- BaekjunCodingTest/src/step1/Test10.java
+++ BaekjunCodingTest/src/step1/Test10.java
@@ -1,9 +1,19 @@
 package step1;
 
 //2588번 곱셈문제
+//(1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Test10 {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        System.out.println(c * (d % 10));            // 10으로 나눠준 값의 나머지 값
+        System.out.println(c * (d % 100 / 10));        // 100으로 나눠준 값의 나머지를 10으로 나눈 
+        System.out.println(c * (d / 100));            // 100으로 나눠준 값
+        System.out.println(c * d);
+    }
 }
--- BaekjunCodingTest/src/step2/Test03.java
+++ BaekjunCodingTest/src/step2/Test03.java
@@ -1,19 +1,23 @@
 package step2;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-//두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+//2753번 연도가 주어졌을 때 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오
 
 public class Test03 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		Scanner sc = new Scanner(System.in);
+		int year = Integer.parseInt(br.readLine());
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		
-		System.out.println(a-b);
-	
-		sc.close();
+		if(year>=1 && year<=4000) {
+			if(year%4 == 0 && year%100!=0 || year % 400 == 0) {
+				System.out.println(1);
+			}else {
+				System.out.println(0);
+			}
+		}
 	}
 }
